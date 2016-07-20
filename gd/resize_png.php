5193225c79d5c9fc12771ed570a61e65d16083c2
Milena Tomova <kokolina1888@gmail.com>
2016-07-20 20:39:42 +0300

    sinc

--- gd/resize_png.php
+++ gd/resize_png.php
@@ -1,22 +1,26 @@
 <?php 
-$src = 'images.jpg';
-$new_file = 'test_new.jpg';
-$width = 200;
-$height = 200;
-$rgb = 0xffffff;
+$src = 'water_PNG3290.png';
+$new_file = 'test_png.png';
+$width = 400;
+$height = 400;
+//$rgb = 0xffffff;
 $size = getimagesize($src);
 $x_ratio = $width/$size[0];
 $y_ratio = $height/$size[1];
 $ratio = min($x_ratio, $y_ratio);
+
 $use_x_ratio = ($x_ratio==$ratio);
 $new_width = $use_x_ratio ? $width : floor($size[0] * $ratio);
 $new_height = !$use_x_ratio ? $height : floor($size[1] * $ratio);
 $new_left = $use_x_ratio ? 0 : floor(($width-$new_width)/2);
 $new_top = !$use_x_ratio ? 0 : floor(($height-$new_height)/2);
 $img = imagecreatetruecolor($width, $height);
-imagefill($img, 0, 0, $rgb);
-$photo = imagecreatefromjpeg($src);
+//imagefill($img, 0, 0, $rgb);
+imagealphablending($img, false);
+imagesavealpha($img, true);
+
+$photo = imagecreatefrompng($src);
 imagecopyresampled($img, $photo, $new_left, $new_top, 0, 0, $new_width, $new_height, $size[0], $size[1]);
-imagejpeg($img, $new_file);
+imagepng($img, $new_file);
 imagedestroy($img);
 imagedestroy($photo);
